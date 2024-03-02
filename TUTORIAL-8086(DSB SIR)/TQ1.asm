9884105a9a4fc359a44688db336fba9eb5e4ea91
Aashish Karki <078bct004.aashish@pcampus.edu.np>
2024-03-02 19:56:28 +0545

    Solved Tutorial

---
+++ TUTORIAL-8086(DSB SIR)/TQ1.asm
@@ -0,0 +1,59 @@
+TITLE ADD 10 NUMBERS WHICH ARE BETWEEN 50 AND 150 ONLY DISPLAY THE SUM IN DECIMAL
+.MODEL SMALL
+.STACK
+.DATA
+    ARR  DW 10,70,26,79,70,30,130,40,100,150
+    STRING  DB "THE SUM OF NUMBERS IS: $"
+    SUM DW ?
+.CODE
+MAIN PROC FAR
+           MOV  AX,@DATA
+           MOV  DS,AX
+           MOV  AX,@STACK
+           MOV  SS,AX
+
+           LEA  SI,ARR
+           MOV  AX,0; SUM=0
+           MOV  CX,10; Counter
+           ;Summing the numbers
+    REPEAT:
+           CMP [SI],50
+           JC SKIP
+           CMP [SI],150
+           JNC  SKIP
+           ADD  AX,[SI]
+        SKIP:INC  SI
+           INC SI
+           LOOP REPEAT
+
+        ;For getting the Decimal Value
+           MOV DX,0
+           MOV CX,0
+           MOV BX,10D; For getting Decimal Value
+
+    REPEAT2:
+           CMP AX,0
+           JZ NEXT ;Break condition if done with all digits
+           DIV BX   ;Dividing the AX by 10, DX:AX/BX
+           PUSH DX ;Pushing the Remainder DX:AX Remainder:Quotient
+           SUB DX,DX ;Clearing the DX
+           INC CX   ; Count for popping later on
+           JMP REPEAT2
+
+    NEXT:
+            CMP CX,0
+            JZ EXIT
+            POP DX
+            ADD DX,30H
+            DEC CX
+            MOV AH,02H
+            INT 21H
+            JMP NEXT
+
+    EXIT:
+           MOV  AX,4C00H
+           INT  21H
+
+
+MAIN ENDP
+    END MAIN
---
+++ TUTORIAL-8086(DSB SIR)/TQ5.asm
@@ -0,0 +1,37 @@
+TITLE TO TAKE INPUT AND DISPLAY ON 10,20 ON SCREEN
+.MODEL SMALL
+.DATA
+    MAXLEN  DB 50
+    ACTLEN  DB ?
+    STRING1 DB 50 DUP('$')
+    ROW     DB 10
+    COL     DB 20
+
+.CODE
+MAIN PROC FAR
+         MOV AX,@DATA
+         MOV DS,AX  
+         ;TAKE INPUT
+         MOV AH,0AH
+         LEA DX,MAXLEN
+         INT 21H
+        ;REQUEST VIDEO MODE
+         MOV AH,00H
+         MOV AL,03H
+         INT 10H
+        ;SET CURSOR POSITION
+         MOV AH,02H
+         MOV DH,ROW
+         MOV DL,COL
+         INT 10H
+        ;DISPLAY STRING
+         LEA DX,STRING1
+         MOV AH,09H
+         INT 21H
+
+         MOV AX,4C00H
+         INT 21H
+
+MAIN ENDP
+    END MAIN
+
